9f9f37e37e2c4b7ced5355072a6b8aa4992652d9
Thomas Dekiere <t.dekiere@gmail.com>
2022-01-10 07:47:46 +0100

    fix: use env variable for gcp_project

--- packages/service/src/build-details.ts
+++ packages/service/src/build-details.ts
@@ -1,29 +1,29 @@
 import {CloudBuildClient} from '@google-cloud/cloudbuild';
+import { config } from './config';
 
 // Creates a client
 const cb = new CloudBuildClient();
 
 export const getBuild = async (id: string) => {
-  console.log('Requesting build: ' + id);
   const [build] = await cb
     .getBuild({
       id,
-      projectId: 'express-dev',
+      projectId: config.GCP_PROJECT,
     })
     .catch(error => {
-      console.error(error);
+      console.error(`Failed to get build: ${id} in project ${config.GCP_PROJECT}`);
       throw error;
     });
-
+    
   const [trigger] =
     (build.buildTriggerId &&
       (await cb
         .getBuildTrigger({
           triggerId: build.buildTriggerId,
-          projectId: 'express-dev',
+          projectId: config.GCP_PROJECT,
         })
         .catch(error => {
-          console.error(error);
+          console.error(`Failed to get build trigger: ${build.buildTriggerId} in project ${config.GCP_PROJECT}`);
           throw error;
         }))) ||
     [];
