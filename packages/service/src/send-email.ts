66dbcbe106ff75e4e50480bddf14723a11803e5c
Thomas Dekiere <t.dekiere@gmail.com>
2022-01-10 12:18:36 +0100

    fix: missing subject in email

--- packages/service/src/send-email.ts
+++ packages/service/src/send-email.ts
@@ -1,21 +1,21 @@
-import sgMail, {MailDataRequired} from '@sendgrid/mail';
-import {getBuild} from './build-details';
-import {config} from './config';
-import {getCommitInfo} from './git';
+import sgMail, { MailDataRequired } from '@sendgrid/mail';
+import { getBuild } from './build-details';
+import { config } from './config';
+import { getCommitInfo } from './git';
 
 
 const sendEmail = async (emailData: MailDataRequired) => {
   sgMail.setApiKey(config.SENDGRID_API_KEY);
 
   return sgMail
-    .sendMultiple(emailData)
+    .send(emailData)
     .then(() => {
-      console.log('Emails sent');
+      console.log('Email sent');
     })
     .catch(error => {
+      console.error("Failed to send email");
       console.log({
         emailData,
-        dynamicData: emailData.personalizations
       });
       console.error(error);
     });
@@ -39,21 +39,17 @@ export const sendBuildReportEmail = async ({
   const templateId = config.SENDGRID_TEMPLATE_PREVIEW_BUILD_STATUS;
 
   await sendEmail({
-    from: config.SENDGRID_SENDER,
     templateId,
-    personalizations: [
-      {
-        to: [ 't.dekiere@gmail.com', commit.author.email ],
-        dynamicTemplateData: {
-          subject: `Build ${status}: ${build.source.branchName} (${build.trigger?.name})`,
-          trigger: build.trigger?.name,
-          branch: build.source.branchName,
-          sha: build.source.commitSha,
-          status: status.toLowerCase(),
-          issueNr: issue,
-          commitAuthor: commit.author.name,
-        },
-      },
-    ],
+    from: config.SENDGRID_SENDER,
+    to: [ commit.author.email],
+    dynamicTemplateData: {
+      subject: `Build ${build.source.branchName}: ${status.toLowerCase()} (${build.trigger?.name})`,
+      trigger: build.trigger?.name,
+      branch: build.source.branchName,
+      sha: build.source.commitSha,
+      status: status.toLowerCase(),
+      issueNr: issue,
+      commitAuthor: commit.author.name,
+    },
   });
 };
