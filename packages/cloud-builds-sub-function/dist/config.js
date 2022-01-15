d82d74d6d8ab2b7331805e22ba7cf0ec92cef313
Thomas Dekiere <t.dekiere@gmail.com>
2022-01-15 18:08:10 +0100

    fix: add cloudbuild yaml back

--- packages/cloud-builds-sub-function/dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-const yenv = require('yenv');
-const env = yenv('config.yaml');
-exports.config = {
-    GCP_PROJECT: env.GCP_PROJECT,
-};
