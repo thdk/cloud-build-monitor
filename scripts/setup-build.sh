d46afa9e3fa351e7cac53b6c141dc77b7bf669d6
Thomas Dekiere <t.dekiere@gmail.com>
2022-01-16 19:09:22 +0100

    ci: wip

--- scripts/setup-build.sh
+++ scripts/setup-build.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-serviceAccount="ciccd-builder@${PROJECT_ID}.iam.gserviceaccount.com"
+# serviceAccount="ciccd-builder@${PROJECT_ID}.iam.gserviceaccount.com"
 
-echo -e "${ICON} create a service account to be used by cloud builds"
-gcloud iam service-accounts create ciccd-builder \
-    --display-name="ciccd-builder"
+# echo -e "${ICON} create a service account to be used by cloud builds"
+# gcloud iam service-accounts create ciccd-builder \
+#     --display-name="ciccd-builder"
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:$serviceAccount" \
-    --role="roles/secretmanager.secretAccessor"
+# gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+#     --member="serviceAccount:$serviceAccount" \
+#     --role="roles/secretmanager.secretAccessor"
 
-echo -e "${ICON} create build logs bucket"
-gcloud logging buckets create ciccd-build-logs --location=${REGION}
+# echo -e "${ICON} create build logs bucket"
+# gcloud logging buckets create ciccd-build-logs --location=${REGION}
 
-echo -e "${ICON} allow service account to write to logs bucket"
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:$serviceAccount" \
-    --role="roles/logging.bucketWriter"
+# echo -e "${ICON} allow service account to write to logs bucket"
+# gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+#     --member="serviceAccount:$serviceAccount" \
+#     --role="roles/logging.bucketWriter"
 
-gcloud projects add-iam-policy-binding ${PROJECT_ID} \
-    --member="serviceAccount:$serviceAccount" \
-    --role="roles/storage.objectAdmin"   
+# gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+#     --member="serviceAccount:$serviceAccount" \
+#     --role="roles/storage.objectAdmin"   
 
 # NOTE: this step requires you have forked the original repo
 # Your github repo must be connected with Google Source Repositories: https://console.cloud.google.com/cloud-build/repos
@@ -30,13 +30,13 @@ gcloud beta builds triggers create github \
     --repo-owner="${REPO_OWNER}" \
     --build-config="cloudbuild.yaml" \
     --branch-pattern=".*" \
-    --service-account="projects/${PROJECT_ID}/serviceAccounts/${serviceAccount}" \
     --name="ci-build"
+    # --service-account="projects/${PROJECT_ID}/serviceAccounts/${serviceAccount}" \
 
 gcloud beta builds triggers create github \
     --repo-name="cloud-build-monitor" \
     --repo-owner="${REPO_OWNER}" \
     --build-config="cloudbuild-deploy.yaml" \
     --branch-pattern="master" \
-    --service-account="projects/${PROJECT_ID}/serviceAccounts/${serviceAccount}" \
     --name="deploy"
+    # --service-account="projects/${PROJECT_ID}/serviceAccounts/${serviceAccount}" \
