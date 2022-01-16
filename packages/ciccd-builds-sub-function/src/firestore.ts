467be3fbfb270d0d0e3079c378e1dc0300bab945
Thomas Dekiere <t.dekiere@gmail.com>
2022-01-16 19:26:55 +0100

    feat: save build info in firestore

---
+++ packages/ciccd-builds-sub-function/src/firestore.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore, FieldValue } from 'firebase-admin/firestore';
+import { CICCDBuild } from './interfaces';
+
+initializeApp();
+
+const db = getFirestore();
+
+export async function addOrUpdateCICCDBuild(data: CICCDBuild) {
+    const buildsCollectionRef = db.collection('builds');
+    const document = await buildsCollectionRef.doc(data.id).get();
+
+    await document.exists
+        ? document.ref.update({
+            updated: FieldValue.serverTimestamp(),
+            ...data,
+        })
+        : document.ref.set({
+            ...data,
+            created: FieldValue.serverTimestamp(),
+            updated: FieldValue.serverTimestamp(),
+        });
+}
--- packages/ciccd-builds-sub-function/src/index.ts
+++ packages/ciccd-builds-sub-function/src/index.ts
@@ -3,31 +3,46 @@ import type { EventFunction } from '@google-cloud/functions-framework';
 import { sendBuildReportEmail } from './send-email';
 import { getCommitInfo } from './git';
 import {config} from './config';
+import { addOrUpdateCICCDBuild } from './firestore';
 
 
 export const ciccdBuildEvents: EventFunction = async ({
     attributes,
 }: PubsubMessage) => {
     const {
+        id,
         name,
         status,
         commitSha,
         branchName,
+        origin,
+        repo,
     } = attributes || {};
 
-    if (status !== "success" && status != "failure") {
-        return;
-    }
-
+    
     const commit = await getCommitInfo(commitSha);
-
+    
     const issue = commit.message.match(new RegExp(config.ISSUE_REGEX));
-    await sendBuildReportEmail({
-        branch: branchName,
-        author: commit.author.email,
-        issueNr: issue ? issue[0] : null,
-        sha: commitSha,
-        status,
-        trigger: name,
-    })
+    
+    await Promise.all([
+        addOrUpdateCICCDBuild({
+            branchName,
+            commitSha,
+            name,
+            origin,
+            repo,
+            status,
+            id,
+        }),
+        status !== "success" && status != "failure" 
+            ? undefined
+            :  sendBuildReportEmail({
+                branch: branchName,
+                author: commit.author.email,
+                issueNr: issue ? issue[0] : null,
+                sha: commitSha,
+                status,
+                trigger: name,
+            }),
+    ]);
 };
