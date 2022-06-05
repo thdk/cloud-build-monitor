58ecc6ff178eea8aed89328c75b67c249110ffbb
Thomas Dekiere <t.dekiere@gmail.com>
2022-06-05 22:16:39 +0200

    ci: use standalone nextjs output to reduce image size

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
       "avatars.githubusercontent.com",
     ],
+  },
+  experimental: {
+    outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, '../../'),
   },
 };
 
