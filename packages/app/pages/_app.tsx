4e15f6d39761e5085982af353994bbc373e2ee9f
Thomas Dekiere <t.dekiere@gmail.com>
2022-06-09 07:53:01 +0200

    chore: add loging when firebase env variable is not defined

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -6,21 +6,12 @@ import NextNProgress from 'nextjs-progressbar';
 import { useState } from 'react';
 
 import 'antd/dist/antd.css';
+import { intitializeFirebase } from '../firebase/init-firebase';
 
-import { FirebaseOptions, initializeApp } from "firebase/app";
-
-const config: FirebaseOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
-initializeApp(config);
+intitializeFirebase();
 
 function App({ Component, pageProps }: AppProps) {
+
   const [queryClient] = useState(() => new QueryClient())
   return (
     <>
