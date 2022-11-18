b30471c0cab845de6428e45e3ba4a608b1a76c06
Thomas Dekiere <t.dekiere@gmail.com>
2022-11-18 05:54:55 +0100

    chore: move db actions to firestore layer

--- packages/app/features/configs/firestore.ts
+++ packages/app/features/configs/firestore.ts
@@ -1,5 +1,5 @@
-import { QueryDocumentSnapshot } from "firebase/firestore";
-import { Config } from "./types";
+import { collection, getDocs, getFirestore, query, QueryDocumentSnapshot, where } from "firebase/firestore";
+import { Config, ConfigSection } from "./types";
 
 export const CONFIG_COLLECTION = "configs" as const;
 
@@ -14,4 +14,18 @@ export const configConverter = {
             id: docData.id,
         } as unknown as Config;
     },
+};
+
+export const getAllConfigsQuery = collection(getFirestore(), CONFIG_COLLECTION);
+
+export const getAllConfigs = () => getDocs(getAllConfigsQuery);
+
+export const createFindConfigsBySectionQuery = (section?: ConfigSection) => {
+    let baseQuery = query(getAllConfigsQuery);
+    
+    if (section) {
+        baseQuery = query(baseQuery, where("section", "==", section));
+    }
+
+    return baseQuery;
 };
