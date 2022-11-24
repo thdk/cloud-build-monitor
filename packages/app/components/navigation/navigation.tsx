e51dd17cb3b694e5c3085ece03b0d4ffe5ca8733
Thomas Dekiere <t.dekiere@gmail.com>
2022-11-24 07:15:00 +0100

    feat: use a single vertical menu for all pages and tabs

--- packages/app/components/navigation/navigation.tsx
+++ packages/app/components/navigation/navigation.tsx
@@ -1,28 +1,102 @@
-import { NavigationLink } from "./navigation-link";
+import { Build, Settings, Source } from "@mui/icons-material";
+import { Menu, MenuProps } from "antd";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const items: MenuProps['items'] = [
+    {
+        label: 'Builds',
+        key: '/builds',
+        icon: <Build />
+    },
+    {
+        label: 'Repos',
+        key: '/repos',
+        icon: <Source />
+    },
+    {
+        key: '/config',
+        label: 'Config',
+        icon: <Settings />,
+        children: [
+            {
+                label: 'Builds',
+                key: '/config/builds',
+            },
+            {
+                label: 'General',
+                key: '/config/general',
+            },
+        ],
+    },
+];
+
+
 
 export function Navigation() {
+    const { push, asPath } = useRouter();
+
+    const onClick: MenuProps['onClick'] = e => {
+        push(
+            e.key,
+            undefined,
+            {
+                shallow: false,
+            }
+        );
+    };
+
+    const onOpenChange: MenuProps['onOpenChange'] = e => {
+        setOpenKeys(e);
+    };
+
+    const [openKeys, setOpenKeys] = useState([asPath]);
+    const [activeKey, setActiveKey] = useState(asPath);
+
+    useEffect(
+        () => {
+            setActiveKey(asPath);
+            setOpenKeys(
+                asPath.split('/').reduce<string[]>(
+                    (p, c, i) => {
+                        switch (i) {
+                            case 0:
+                                break;
+                            case 1:
+                                p.push(`/${c}`);
+                                break;
+                            default:
+                                p.push(`${p[i - 2]}/${c}`);
+                                break;
+                        }
+
+                        return p;
+                    },
+                    []
+                )
+            );
+        },
+        [
+            asPath,
+        ],
+    );
+
     return (
         <div
-            className="flex flex-col pt-32 fixed"
+            style={{
+                width: 184,
+                marginLeft: 8,
+                marginRight: 8.
+            }}
+            className="pt-32 fixed"
         >
-            <NavigationLink
-                href="/"
-                label="Home"
-            />
-
-            <NavigationLink
-                href="/builds"
-                label="Builds"
-            />
-
-            <NavigationLink
-                href="/repos"
-                label="Repos"
-            />
-
-            <NavigationLink
-                href="/config/builds"
-                label="Config"
+            <Menu
+                activeKey={activeKey}
+                onClick={onClick}
+                mode="inline"
+                items={items}
+                openKeys={openKeys}
+                onOpenChange={onOpenChange}
             />
         </div>
     );
--- packages/app/pages/config/[[...tab]].tsx
+++ packages/app/pages/config/[[...tab]].tsx
@@ -1,50 +1,14 @@
-import { Settings, Build } from "@mui/icons-material";
-import { Menu } from "antd";
-import type { MenuProps } from 'antd';
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import { Layout } from "../../components/layout";
 import { ConfigScreen } from "../../features/configs";
 import { ArtifactScreen } from "../../components/artifact-screen/artifact-screen";
 
-const items: MenuProps['items'] = [
-    {
-        label: 'Builds',
-        key: 'builds',
-        icon: <Build />,
-    },
-    {
-        label: 'General',
-        key: 'general',
-        icon: <Settings />,
-    },
-];
-
 const ConfigPage: NextPage<{ tab: string }> = ({
     tab,
 }) => {
-
-    const { pathname, query, replace } = useRouter();
-    const [current, setCurrent] = useState(tab);
-
-    const onClick: MenuProps['onClick'] = e => {
-        replace({
-            pathname,
-            query: {
-                ...query,
-                tab: e.key,
-            },
-        });
-    };
-
-    useEffect(() => {
-        setCurrent(tab);
-    }, [tab])
-
     return (
         <Layout>
-            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+           
             {
                 tab === "builds"
                     ? <ArtifactScreen />
