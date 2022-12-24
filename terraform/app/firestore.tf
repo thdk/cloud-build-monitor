c82f6d8c4937c021185b4d7de151d3e36dd83f48
Thomas Dekiere <t.dekiere@gmail.com>
2022-12-24 08:09:37 +0100

    infra: publish new version of firestore rules

--- terraform/app/firestore.tf
+++ terraform/app/firestore.tf
@@ -21,15 +21,51 @@ resource "google_firestore_document" "config" {
 
 ## Firestore rules
 data "local_file" "firestore-rules" {
-    filename = "${path.module}/firestore.rules"
+  filename = "${path.module}/firestore.rules"
 }
 resource "google_firebaserules_ruleset" "default" {
   source {
     files {
-      content = data.local_file.firestore-rules.content
-      name    = "firestore.rules"
+      content     = data.local_file.firestore-rules.content
+      name        = "firestore.rules"
       fingerprint = sha1(data.local_file.firestore-rules.content)
     }
+  }
+
+  lifecycle {
+    ignore_changes  = all
+  }
+
+  project = var.project
+}
+
+resource "google_firebaserules_ruleset" "rules-2022-12-24" {
+  source {
+    files {
+      content     = data.local_file.firestore-rules.content
+      name        = "firestore.rules"
+      fingerprint = sha1(data.local_file.firestore-rules.content)
+    }
+  }
+
+  lifecycle {
+    ignore_changes  = all
+  }
+
+  project = var.project
+}
+
+resource "google_firebaserules_ruleset" "rules-2022-12-24-2" {
+  source {
+    files {
+      content     = data.local_file.firestore-rules.content
+      name        = "firestore.rules"
+      fingerprint = sha1(data.local_file.firestore-rules.content)
+    }
+  }
+
+  lifecycle {
+    ignore_changes  = all
   }
 
   project = var.project
@@ -37,10 +73,19 @@ resource "google_firebaserules_ruleset" "default" {
 
 resource "google_firebaserules_release" "default" {
   name         = "production"
-  ruleset_name = "projects/${var.project}/rulesets/${google_firebaserules_ruleset.default.name}"
+  ruleset_name = "projects/${var.project}/rulesets/${google_firebaserules_ruleset.rules-2022-12-24-2.name}"
   project      = var.project
   depends_on = [
-    google_firebaserules_ruleset.default,
+    google_firebaserules_ruleset.rules-2022-12-24-2,
+  ]
+}
+
+resource "google_firebaserules_release" "primary" {
+  name         = "cloud.firestore"
+  ruleset_name = "projects/${var.project}/rulesets/${google_firebaserules_ruleset.rules-2022-12-24-2.name}"
+  project      = var.project
+  depends_on = [
+    google_firebaserules_ruleset.rules-2022-12-24-2,
   ]
 }
 
