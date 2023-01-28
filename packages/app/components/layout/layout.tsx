d711f7652c04cf56bad3f4bd9baa088754337bb4
Thomas Dekiere <t.dekiere@gmail.com>
2023-01-28 18:16:16 +0100

    feat(app): add report issue button in header

--- packages/app/components/layout/layout.tsx
+++ packages/app/components/layout/layout.tsx
@@ -1,11 +1,11 @@
 import { PropsWithChildren } from "react";
 import { Navigation } from "../navigation";
-import { ConfigProvider, Layout as AntLayout } from "antd";
+import { Button, Col, ConfigProvider, Layout as AntLayout, Row } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
 const { Header, Content } = AntLayout;
 
 const headerStyle: React.CSSProperties = {
     paddingInline: 0,
-    justifyContent: "center",
 };
 
 export function Layout({
@@ -16,13 +16,48 @@ export function Layout({
             theme={{
                 token: {
                     borderRadius: 2,
-                    // colorBgLayout: '#00b96b',
                 },
             }}
         >
             <AntLayout style={{ minHeight: '100vh' }}>
                 <Header style={headerStyle}>
-                    <Navigation />
+                    <Row
+                        align="middle"
+                    >
+                        <Col flex={20}>
+                            <Navigation />
+                        </Col>
+                        <Col flex={4}
+                            md={4}
+                            sm={0}
+                            xs={0}
+
+                            style={{
+                                color: "white",
+
+                            }}
+                        >
+                            <Row
+                                justify="end"
+                                align="middle"
+                                style={{
+                                    paddingRight: "2em"
+                                }}
+                            >
+
+                                <Button
+                                    icon={<GithubOutlined style={{
+                                        verticalAlign: 'middle',
+                                    }} />}
+                                    href={"https://github.com/thdk/cloud-build-monitor/issues"}
+                                    size="small"
+                                >
+                                    Report an issue
+                                </Button>
+                            </Row>
+                        </Col>
+                    </Row>
+
                 </Header>
                 <Content
                 >
