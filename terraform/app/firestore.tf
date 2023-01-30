663e28eaec190e9808cfa8e08be2df14feca26c4
Thomas Dekiere <t.dekiere@gmail.com>
2023-01-30 07:22:01 +0100

    infra: cleanup old firestore rulesets

--- terraform/app/firestore.tf
+++ terraform/app/firestore.tf
@@ -23,52 +23,6 @@ resource "google_firestore_document" "config" {
 data "local_file" "firestore-rules" {
   filename = "${path.module}/firestore.rules"
 }
-resource "google_firebaserules_ruleset" "rules-2022-12-24-2" {
-  source {
-    files {
-      content     = data.local_file.firestore-rules.content
-      name        = "firestore.rules"
-      fingerprint = sha1(data.local_file.firestore-rules.content)
-    }
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-
-  project = var.project
-}
-
-resource "google_firebaserules_ruleset" "rules-2023-01-27" {
-  source {
-    files {
-      content     = data.local_file.firestore-rules.content
-      name        = "firestore.rules"
-      fingerprint = sha1(data.local_file.firestore-rules.content)
-    }
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-
-  project = var.project
-}
-
-resource "google_firebaserules_ruleset" "rules-2023-01-27-2" {
-  source {
-    files {
-      content = data.local_file.firestore-rules.content
-      name    = "firestore.rules"
-    }
-  }
-
-  lifecycle {
-    ignore_changes = all
-  }
-
-  project = var.project
-}
 
 resource "google_firebaserules_ruleset" "rules" {
   source {
